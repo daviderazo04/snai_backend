d4f63c793f989fbb84af75104894328c3b25780c
Alfonso Chafla <117238753+Luden20@users.noreply.github.com>
2026-01-14 21:13:16 -0500

    Me olvide los modulos

--- src/reporteria/reporteria.module.ts
+++ src/reporteria/reporteria.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Adolescente } from '../adolescente/entities/adolescente.entity';
 import { ReportesDemograficosService } from './services/reportes.demograficos.service';
 import { ReporteriaController } from './controllers/reporteria.controller';
+import { UsuarioModule } from '../usuario/usuario.module';
+import { AuditoriaModule } from '../auditoria/auditoria.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Adolescente])],
+  imports: [
+    TypeOrmModule.forFeature([Adolescente]),
+    UsuarioModule,
+    AuditoriaModule,
+  ],
   providers: [ReportesDemograficosService],
   controllers: [ReporteriaController],
 })
